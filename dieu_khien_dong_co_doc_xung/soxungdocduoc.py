4759943bb949ae38c7e4bffacad98a6d2f36a49e
nguyendo1403 <131886442+nguyendo1403@users.noreply.github.com>
2023-05-25 20:23:42 +0700

    Update soxungdocduoc.py

--- dieu_khien_dong_co_doc_xung/soxungdocduoc.py
+++ dieu_khien_dong_co_doc_xung/soxungdocduoc.py
@@ -145,70 +145,33 @@ def gui_doc_du_lieu_dc2():
     print("xungdc2 sau mot khoang thoi gian 1 giay la:",xungdc2)           
     r2_3_1 = r2_3_2
           
+# def listenermodbus():
+#     rospy.init_node('listener', anonymous=True)
+#     time.sleep(0.01)
+#     rospy.Subscriber('modbus', String, callback)
+
+# start_time = time.time()
+# while (True):
+#     listenermodbus()
+#     elapsed_time=time.time() - start_time
+#     if elapsed_time >= 1:
+        
+#         gui_doc_du_lieu_dc1()
+#         gui_doc_du_lieu_dc2()      
+        
+#         print("------")
+#         start_time = time.time()
+
 def listenermodbus():
     rospy.init_node('listener', anonymous=True)
-    time.sleep(0.01)
     rospy.Subscriber('modbus', String, callback)
 
-start_time = time.time()
-while (True):
+def main():
     listenermodbus()
-    elapsed_time=time.time() - start_time
-    if elapsed_time >= 1:
-        
+    while True:
         gui_doc_du_lieu_dc1()
-        gui_doc_du_lieu_dc2()
-        
-        # # client = ModbusClient(method='rtu', port='/dev/ttyUSB0', stopbits=1, parity='N', baudrate=19200)
-        # builder1 = BinaryPayloadBuilder(byteorder=Endian.Big, wordorder=Endian.Big)
-        # builder1.reset()
-        # builder1.add_16bit_int(int(v11))
-        # payload1 = builder1.to_registers()
-        # w1_3 = client.write_register(0x30, payload1[0],unit=0x01)
-    
-        # builder2 = BinaryPayloadBuilder(byteorder=Endian.Big, wordorder=Endian.Big)
-        # builder2.reset()
-        # builder2.add_16bit_int(int(v22))
-        # payload2=builder2.to_registers()
-        # w2_3 = client.write_register(0x30, payload2[0],unit=0x02)
-    
-        # if (v11==0) and (v22==0):
-        #     w1_3 = client.write_register(0x30, 0,unit=0x01)
-        #     w2_3 = client.write_register(0x30, 0,unit=0x02)
-        
-        # print("done gui du lieu",v11,v22)
-        
-        
-        # #đọc giá trị xung tiếp theo cho động cơ 1
-        # readxungdc1 = client.read_holding_registers(0x04,2,unit=0x01)
-        # r1_1 = str(hex(readxungdc1.registers[int(0)]))
-        # r1_2 = str(hex(readxungdc1.registers[int(1)]))
-        # r1_1 = r1_1[2:]
-        # r1_2 = r1_2[2:]
-        # if len(r1_2) == 3:
-        #     r1_2 = "0" + r1_2
-        # r1_3_2 = int(r1_1 + r1_2,base=16)
-        
-        # xungdc1 = abs(r1_3_2 - r1_3_1) #xung của động cơ 1 sau các khoảng thời gian đọc được
-        # print("xungdc1 sau mot khoang thoi gian la :",xungdc1)           
-        # r1_3_1 = r1_3_2
-        
+        gui_doc_du_lieu_dc2() 
+        time.sleep(1)
 
-        # #đọc giá trị xung tiếp theo cho động cơ 2
-        # readxungdc2 = client.read_holding_registers(0x04,2,unit=0x02)
-        # r2_1 = str(hex(readxungdc2.registers[int(0)]))
-        # r2_2 = str(hex(readxungdc2.registers[int(1)]))
-        # r2_1 = r2_1[2:]
-        # r2_2 = r2_2[2:]
-        # if len(r2_2) == 3:
-        #     r2_2 = "0" + r2_2
-        # r2_3_2=int(r2_2 + r2_1,base=16)
-        # xungdc2 = abs(r2_3_2 - r2_3_1) #xung của động cơ 2 sau các khoảng thời gian đọc được
-        # print("xungdc2 sau mot khoang thoi gian la:",xungdc2)             
-        # r2_3_1 = r2_3_2
-
-
-        print("------")
-        start_time = time.time()
-
-
+if __name__ == '__main__':
+    main()
