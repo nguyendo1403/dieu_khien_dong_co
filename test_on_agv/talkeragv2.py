d911b270d3f8645743b883905e2bb67f8add94d3
nguyendo1403 <131886442+nguyendo1403@users.noreply.github.com>
2023-06-09 08:07:47 +0700

    Add files via upload

---
+++ test_on_agv/talkeragv2.py
@@ -0,0 +1,154 @@
+#!/usr/bin/python3
+
+import rospy
+from std_msgs.msg import String
+import time
+from geometry_msgs.msg import Twist
+
+
+x0 = 0.1
+y0 = 0.1
+v1 = 0.1     #gia tri gui xuong cho dong co 1
+v2 = 0.2     #gia tri gui xuong cho dong co 2
+  
+
+
+def callback(data):
+    pub = rospy.Publisher('modbus', String, queue_size=10)     
+    
+    a = round(data.linear.x,1)
+    b = round(data.angular.z,1)
+    x0= float(b)    
+    y0=float(a)
+
+    #chay thang 
+    if((x0 >= -0.25) and (x0 <= 0.25) and (y0 > 0)):           
+         v1 = (-1)*abs(y0)
+         v2 = abs(y0)
+    #chay lui
+    elif ((x0 >= -0.25) and (x0 <= 0.25) and (y0 <= 0)):              
+        v1 = abs(y0)
+        v2 = (-1)*abs(y0)     
+    #re phai 
+    elif((y0 >= -0.25) and (y0 <= 0.25) and (x0 > 0)):         
+        v1 = (-1)*abs(x0)
+        v2 = abs(x0)/2
+    #re trai  
+    elif((y0 >= -0.25) and (y0 <= 0.25) and (x0 <= 0)):             
+        v1 = (-1)*(abs(x0))/2
+        v2 = abs(x0)
+    #chay thang phai
+    elif((x0>0.25) and (y0>0.25) and (abs(y0)>abs(x0))):   
+        if (abs(x0)+abs(y0)>=1): 
+            v1 = -1
+            v2 = 1/2
+        else:   
+            v1 = (-1)*abs(y0)
+            v2 = abs(y0)/2
+    elif((x0>0.25) and (y0>0.25) and (abs(y0)==abs(x0))):   
+        v1 = (-1)*abs(y0) 
+        v2 = abs(y0)/2
+    elif((x0>0.25) and (y0>0.25) and (abs(y0)<abs(x0))):
+        if (abs(x0)+abs(y0)>=1): 
+            v1 = (-1)*1         #
+            v2 = 1/2                  
+        else:
+            v1 = (-1)*(abs(x0))
+            v2 = (abs(x0))/2 
+    #chay lui phai
+    elif((x0>0.25) and (y0<-0.25) and (abs(x0)>abs(y0))): 
+        if (abs(x0)+abs(y0)>=1):
+            v1=1/2
+            v2=-1
+        else:  
+            v1 = (abs(x0))/2                         
+            v2 = (-1)* (abs(x0))        
+    elif((x0>0.25) and (y0<-0.25) and (abs(x0)==abs(y0))) :        
+        v1 = (abs(x0))/2
+        v2 = (-1)*abs(x0)        
+    elif((x0>0.25) and (y0<-0.25) and (abs(x0)<abs(y0))) : 
+        if (abs(x0)+abs(y0)>=1):
+            v1=1/2
+            v2=-1
+        else:  
+            v1 = (abs(y0))/2                         
+            v2 = (-1)* (abs(y0))
+    #chay lui trai
+    elif((x0<-0.25) and (y0<-0.25) and (abs(x0)<abs(y0))): 
+        if (abs(x0)+abs(y0)>=1):
+            v1=1
+            v2=-1/2
+        else:  
+            v1 = (abs(y0))                        
+            v2 = (-1)* (abs(y0))/2        
+    elif((x0<-0.25) and (y0<-0.25) and (abs(x0)==abs(y0))) :        
+        v1 = (abs(x0))
+        v2 = (-1)*abs(x0)/2       
+    elif((x0<-0.25) and (y0<-0.25) and (abs(x0)>abs(y0))) : 
+        if (abs(x0)+abs(y0)>=1):
+            v1=1
+            v2=-1/2
+        else:  
+            v1 = (abs(x0))                       
+            v2 = (-1)* (abs(x0))/2   
+    #chay thang trai
+    elif((x0<-0.25) and (y0>0.25) and (abs(y0)<abs(x0))):   
+        if (abs(x0)+abs(y0)>=1): 
+            v1 = -1/2
+            v2 = 1
+        else:   
+            v1 = (-1)*abs(x0)/2
+            v2 = abs(x0)
+    elif((x0<-0.25) and (y0>0.25) and (abs(y0)==abs(x0))):   
+        v1 = (-1)*abs(y0)/2 
+        v2 = abs(y0)
+    elif((x0<-0.25) and (y0>0.25) and (abs(y0)>abs(x0))):
+        if (abs(x0)+abs(y0)>=1): 
+            v1 = (-1)/2         #
+            v2 = 1                  
+        else:
+            v1 = (-1)*(abs(y0))/2
+            v2 = (abs(y0))
+    #dung
+    else:
+        v1 = 0
+        v2 = 0
+
+
+    if (x0 == 0) and (y0 == 0):
+        v1 = 0
+        v2 = 0
+       
+       
+    global v11
+    global v22
+        
+    v11=((v1*1000)/1) #xu li van toc cho dong co 1  
+    v22=((v2*1000)/1) #xu li van toc cho dong co 2 
+       
+    v11 = str(v11)
+    v22 = str(v22)
+
+    giatrigui = v11 + "," + v22  
+    print("gia tri gui la:",giatrigui)
+    pub.publish(giatrigui)  
+    
+    print(x0,y0)
+
+
+        
+def talker ():
+    
+    pub = rospy.Publisher('modbus', String, queue_size=10)
+    rospy.init_node('talkermodbus', anonymous=True)
+    rate = rospy.Rate(1)
+    rospy.loginfo("Publisher Node Started, now publishing messages")
+    rospy.Subscriber("cmd_vel", Twist, callback)
+    rospy.spin()
+
+         	
+if __name__== '__main__':
+    try:
+        talker()        
+    except rospy.ROSInterruptException:
+    	pass
